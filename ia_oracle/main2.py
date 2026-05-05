410fd28bfce1cfb9a136a815d1d1fd5c3daf8b46
lcpnzfr <260931567+lcpnzfr@users.noreply.github.com>
2026-05-05 13:59:55 -0300

    feat: add EnvConfigManager bootstrap script and log file for ia_oracle service

--- ia_oracle/main2.py
+++ ia_oracle/main2.py
@@ -7,8 +7,16 @@ from forex_shared.env_config_manager import EnvConfigManager
 from forex_shared.logging.get_logger import get_logger, setup_logging
 
 # Setup basic logging
-setup_logging(level=logging.DEBUG)
+setup_logging(level=logging.INFO)
 logger = get_logger(__name__)
+
+try:
+    EnvConfigManager.startup()
+except Exception as e:
+    logger.exception(f"Failed to sync with MongoDB EnvConfig: {e}")
+    sys.exit(1)
+
+logger.info(f"OK!")
 
 try:
     EnvConfigManager.startup()
